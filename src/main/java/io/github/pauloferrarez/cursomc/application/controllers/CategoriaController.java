b7c943da6b30b45ecaafb0666b9227cc35839d09
Paulo Ferrarez <paulo.ferrarez.filho@gmail.com>
2022-09-03 21:08:57 -0300

    Primeira entidade e relação ao banco h2

--- src/main/java/io/github/pauloferrarez/cursomc/application/controllers/CategoriaController.java
+++ src/main/java/io/github/pauloferrarez/cursomc/application/controllers/CategoriaController.java
@@ -1,15 +1,25 @@
 package io.github.pauloferrarez.cursomc.application.controllers;
 
+import io.github.pauloferrarez.cursomc.domain.model.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
     @GetMapping()
-    public String findAll(){
-        return "ta fununfano";
+    public List<Categoria> findAll(){
+
+        List<Categoria> categorias = new ArrayList<>();
+
+        categorias.add(new Categoria(1, "Informática"));
+        categorias.add(new Categoria(2, "Robótica"));
+
+        return categorias;
     }
 }
---
+++ src/main/java/io/github/pauloferrarez/cursomc/domain/model/Categoria.java
@@ -0,0 +1,23 @@
+package io.github.pauloferrarez.cursomc.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "CATEGORIA")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NOME")
+    private String nome;
+}
